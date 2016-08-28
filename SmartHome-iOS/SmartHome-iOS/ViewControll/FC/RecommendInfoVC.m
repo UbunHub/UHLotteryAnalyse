eb3a5b8356c96ef7b1ec9824d9309a2b84b4c74d
静静地白色外套 <296019487@qq.com>
2016-08-28 17:06:38 +0800

    添加杀号功能

--- SmartHome-iOS/SmartHome-iOS/ViewControll/FC/RecommendInfoVC.m
+++ SmartHome-iOS/SmartHome-iOS/ViewControll/FC/RecommendInfoVC.m
@@ -14,23 +14,31 @@
 
 @property(strong, nonatomic)NSMutableArray *dataArr;
 
-@property (weak, nonatomic) IBOutlet UILabel *geIstrue_lab;
-
-@property (weak, nonatomic) IBOutlet UILabel *shiIsTrue_lab;
-
-@property (weak, nonatomic) IBOutlet UILabel *baiIsTrue_lab;
-
 @property (weak, nonatomic) IBOutlet UILabel *geRecCode_lab;
+
+@property (weak, nonatomic) IBOutlet UILabel *geKillCode_lab;
 
 @property (weak, nonatomic) IBOutlet UILabel *geOutCode_lab;
 
 @property (weak, nonatomic) IBOutlet UILabel *shiRecCode_lab;
 
+@property (weak, nonatomic) IBOutlet UILabel *shiKillCode_lab;
+
 @property (weak, nonatomic) IBOutlet UILabel *shiOutCode_lab;
 
 @property (weak, nonatomic) IBOutlet UILabel *baiRecCode_lab;
 
+@property (weak, nonatomic) IBOutlet UILabel *baiKillCode_lab;
+
 @property (weak, nonatomic) IBOutlet UILabel *baiOutCode_lab;
+
+@property (weak, nonatomic) IBOutlet UILabel *outCountLab;
+
+@property (weak, nonatomic) IBOutlet UILabel *zhiRecMoneLab;
+
+@property (weak, nonatomic) IBOutlet UILabel *zhiKillMoneLab;
+
+
 @end
 
 
@@ -39,7 +47,7 @@
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    _dataTableView.layer.cornerRadius = 6;
+    _dataTableView.layer.cornerRadius = 16;
     _dataTableView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.4];
     self.dataArr = [[NSMutableArray alloc]initWithObjects:@"5",@"10",@"15",@"20",@"25",@"30",@"50",@"100",  nil];
     [self getHttpDataWithProbability:[_dataArr firstObject]];
@@ -52,42 +60,69 @@
 }
 
 -(void)reloadUIWithDic:(NSDictionary*)dataDic{
+
+    _geOutCode_lab.text = [NSString stringWithFormat:@"%@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_ge"]];
     
-    BOOL geIsTrue = [dataDic[@"geIsTrue"] boolValue];
-    _geIstrue_lab.textColor = (geIsTrue)?[UIColor blueColor]:[UIColor redColor];
-    _geIstrue_lab.text = dataDic[@"geIsTrue"];
-    
-    _geOutCode_lab.text = [NSString stringWithFormat:@"实际数字:  %@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_ge"]];
     _geRecCode_lab.text = @"";
     for (NSString *str in dataDic[@"outGeList"]) {
         _geRecCode_lab.text = [NSString stringWithFormat:@"%@ %@",_geRecCode_lab.text,str];
     }
-    BOOL shiIsTrue = [dataDic[@"shiIsTrue"] boolValue];
-    _shiIsTrue_lab.textColor = (shiIsTrue)?[UIColor blueColor]:[UIColor redColor];
-    _shiIsTrue_lab.text = dataDic[@"shiIsTrue"];
+    BOOL geIsTrue = [dataDic[@"geIsTrue"] boolValue];
+    _geRecCode_lab.textColor = (geIsTrue)?[UIColor blueColor]:[UIColor redColor];
     
-    _shiOutCode_lab.text =  [NSString stringWithFormat:@"实际数字:  %@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_shi"]];
+    _geKillCode_lab.text = @"";
+    for (NSString *str in dataDic[@"outNotGeList"]) {
+        _geKillCode_lab.text = [NSString stringWithFormat:@"%@ %@",_geKillCode_lab.text,str];
+    }
+    BOOL geNotIsTrue = [dataDic[@"geNotIsTrue"] boolValue];
+    _geKillCode_lab.textColor = (geNotIsTrue)?[UIColor blueColor]:[UIColor redColor];
+    
+    _shiOutCode_lab.text =  [NSString stringWithFormat:@" %@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_shi"]];
     _shiRecCode_lab.text = @"";
     for (NSString *str in dataDic[@"outShiList"]) {
         _shiRecCode_lab.text = [NSString stringWithFormat:@"%@ %@",_shiRecCode_lab.text,str];
     }
+    BOOL shiIsTrue = [dataDic[@"shiIsTrue"] boolValue];
+    _shiRecCode_lab.textColor = (shiIsTrue)?[UIColor blueColor]:[UIColor redColor];
     
-    BOOL baiIsTrue = [dataDic[@"baiIsTrue"] boolValue];
-    _baiIsTrue_lab.textColor = (baiIsTrue)?[UIColor blueColor]:[UIColor redColor];
-    _baiIsTrue_lab.text = dataDic[@"baiIsTrue"];
+    _shiKillCode_lab.text = @"";
+    for (NSString *str in dataDic[@"outNotShiList"]) {
+        _shiKillCode_lab.text = [NSString stringWithFormat:@"%@ %@",_shiKillCode_lab.text,str];
+    }
+    BOOL shiNotIsTrue = [dataDic[@"shiNotIsTrue"] boolValue];
+    _shiKillCode_lab.textColor = (shiNotIsTrue)?[UIColor blueColor]:[UIColor redColor];
     
-    _baiOutCode_lab.text = [NSString stringWithFormat:@"实际数字:  %@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_bai"]];
+    _baiOutCode_lab.text = [NSString stringWithFormat:@"%@",[[dataDic objectForKey:@"outData"] objectForKey:@"out_bai"]];
     _baiRecCode_lab.text = @"";
     for (NSString *str in dataDic[@"outBaiList"]) {
         _baiRecCode_lab.text = [NSString stringWithFormat:@"%@ %@",_baiRecCode_lab.text,str];
     }
-    //just for test
+    BOOL baiIsTrue = [dataDic[@"baiIsTrue"] boolValue];
+    _baiRecCode_lab.textColor = (baiIsTrue)?[UIColor blueColor]:[UIColor redColor];
+    _baiKillCode_lab.text = @"";
+    for (NSString *str in dataDic[@"outNotBaiList"]) {
+        _baiKillCode_lab.text = [NSString stringWithFormat:@"%@ %@",_baiKillCode_lab.text,str];
+    }
+    BOOL baiNotIsTrue = [dataDic[@"baiNotIsTrue"] boolValue];
+    _baiKillCode_lab.textColor = (baiNotIsTrue)?[UIColor blueColor]:[UIColor redColor];
     
+    _outCountLab.text = [NSString stringWithFormat:@"%@ %@",[[dataDic objectForKey:@"outData"]objectForKey:@"outdate"],[[dataDic objectForKey:@"outData"]objectForKey:@"outNO"]];
+    NSArray *outGeList = dataDic[@"outGeList"];
+    NSArray *outShiList = dataDic[@"outShiList"];
+    NSArray *outBaiList = dataDic[@"outBaiList"];
+    NSArray *outNotGeList = dataDic[@"outNotGeList"];
+    NSArray *outNotShiList = dataDic[@"outNotShiList"];
+    NSArray *outNotBaiList = dataDic[@"outNotBaiList"];
+    _zhiRecMoneLab.text =[NSString stringWithFormat:@"%d元",(int)(2*outGeList.count*outShiList.count*outBaiList.count)];
+    _zhiKillMoneLab.text =[NSString stringWithFormat:@"%d元",(int)(2*(10-outNotGeList.count)*(10-outNotShiList.count)*(10-outNotBaiList.count))];
+  
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
     return 40.0;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
     return _dataArr.count;
 }
 
@@ -107,6 +142,7 @@
     return cell;
     
 }
+
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     NSString *str = [_dataArr objectAtIndex:indexPath.row];
     [self getHttpDataWithProbability:str];
